b933c975cc3df114ceb00779259879d658033417
Karas02000 <gta5369577@naver.com>
2025-12-22 16:02:35 +0900

    내용 추가

--- src/main/java/ch00_starting/Main.java
+++ src/main/java/ch00_starting/Main.java
@@ -1,7 +1,17 @@
 package ch00_starting;
 
 public class Main {
+    /*
+        주석(comment) : Java 프로그램의 경우 폴더 및 파일의 전체 코드를 컴파일링 하는데, 거기서 오류가 하나라도 발생하게 되면 전체 프로그램 실행이 되지 않는다.
+                       하지만 주석처리를 한 부분에 대해서는 컴퓨터가 코드로 인지하지 않기 때문에 오류가 발생하지 않습니다.
+
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int scoreEnglish = 100;
+        int scoreKorean = 75;
+        System.out.println("영어 점수 : " + scoreEnglish);
+        System.out.println("국어 점수(수정전) : " + scoreKorean);
+        scoreKorean = 90;
+        System.out.println("국어점수(수정후) : " + scoreKorean);
     }
 }
