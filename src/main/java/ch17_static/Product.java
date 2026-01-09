ce54613d400576ff2156cc46ee514d00fc5cecb2
Karas02000 <gta5369577@naver.com>
2026-01-09 12:42:43 +0900

    2026/01/09 | 12:42

--- src/main/java/ch17_static/Product.java
+++ src/main/java/ch17_static/Product.java
@@ -1,4 +1,22 @@
 package ch17_static;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Product {
+    // field 선언
+    private String name;
+    private int instanceCnt = 0;
+    // 정적 변수 선언
+    private static int cnt = 0;
+    @Getter
+    private static String title = "Produc";
+
+    public Product() {
+        System.out.println("Pruduct 클래스의 객체가 생성되었습니다.");
+        instanceCnt++;
+        cnt++;
+    }
 }
--- src/main/java/ch17_static/ProductMain.java
+++ src/main/java/ch17_static/ProductMain.java
@@ -1,5 +1,8 @@
 package ch17_static;
 
 public class ProductMain {
-
+    public static void main(String[] args) {
+        System.out.println(Product.getTitle());
+        // 객체 생성을 하지 않았지만, title 정적 변수를 불러올 수 있었다.
+    }
 }
